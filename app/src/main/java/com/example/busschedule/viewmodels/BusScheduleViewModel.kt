cc4f291d1185f2652ca8be8627ef4d3f94a39c93
Administrator <godxuche@gmail.com>
2021-12-08 14:46:11 +0100

    implemented the viewModel class and created a viewModel factory for creating and instantiating the viewModel

--- app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
+++ app/src/main/java/com/example/busschedule/viewmodels/BusScheduleViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.busschedule.viewmodels
 
-class BusScheduleViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.room.Dao
+import com.example.busschedule.database.schedule.Schedule
+import com.example.busschedule.database.schedule.ScheduleDao
+
+class BusScheduleViewModel(private val scheduleDao: ScheduleDao): ViewModel() {
+    fun fullSchedule(): List<Schedule> = scheduleDao.getAll()
+
+    fun scheduleForStopName(name: String): List<Schedule> = scheduleDao.getByStopName(name)
 }
